cd8fe0f01913d035725084e272e8652fa26c6e58
Gaurav Lalchandani <glalchandanig@gmail.com>
2016-07-27 23:48:22 +0530

    JFrame window created

---
+++ Game.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Game {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Mini Game");
+		frame.setSize(400, 400);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
