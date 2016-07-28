8c0bc561a73bd8852d02e5b66a9a24fbe194b595
Gaurav Lalchandani <glalchandanig@gmail.com>
2016-07-28 11:41:42 +0530

    Rebound Ball feature added

--- Game.java
+++ Game.java
@@ -7,9 +7,52 @@ import javax.swing.JFrame;
 public class Game extends JPanel{
 	
 	int x, y;
-	Game(){
-		x = 0;		//These are the inital positions of the ball that we see
-		y = 0;
+	int mx,my;
+	Game(int x, int y){
+		this.x = x;		//These are the inital positions of the ball that we see
+		this.y = y;
+		mx = my = 1;	//Movement along x and y directions (steps)
+	}
+	
+	public void moveBall(){
+		if(mx > 0){
+			//Movement along SE or NE Direction
+			if(x + mx < getWidth()-30){
+				x += mx;
+			}
+			else{
+				mx = -mx;
+				x += mx;
+			}
+		}
+		else{
+			if(x + mx > 0){
+				x += mx;
+			}
+			else{
+				mx = -mx;
+				x += mx;
+			}
+		}
+		if(my > 0){
+			//Movement along SE or SW direction
+			if(y+my < getHeight()-30){
+				y += my;
+			}
+			else{
+				my = -my;
+				y += my;
+			}
+		}
+		else{
+			if(y+my > 0){
+				y += my;
+			}
+			else{
+				my = -my;
+				y += my;
+			}
+		}
 	}
 	
 	public void paint(Graphics g){
@@ -22,18 +65,17 @@ public class Game extends JPanel{
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Mini Game");
-		Game game = new Game();
+		Game game = new Game((int)(Math.random()*1000)%(300-40), 0);
 		frame.add(game);
-		frame.setSize(400, 400);
+		frame.setSize(300, 400);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		//so that the program exits/terminates on closing
 		
-		while(game.x < 400 && game.y < 400){
-			game.x++;
-			game.y++;
+		while(true){
+			game.moveBall();
 			game.repaint();
 			try{
-				Thread.sleep(10);
+				Thread.sleep(6);
 			}
 			catch(InterruptedException ie){
 				System.out.println(ie);
