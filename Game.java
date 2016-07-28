a8b5e887ef1d6c396d4e511a2b63da600f0ddf9c
Gaurav Lalchandani <glalchandanig@gmail.com>
2016-07-28 08:42:45 +0530

    Ball added

--- Game.java
+++ Game.java
@@ -1,10 +1,43 @@
+import java.awt.*;
+import java.applet.*;
+
+import javax.swing.JPanel;
 import javax.swing.JFrame;
 
-public class Game {
+public class Game extends JPanel{
+	
+	int x, y;
+	Game(){
+		x = 0;		//These are the inital positions of the ball that we see
+		y = 0;
+	}
+	
+	public void paint(Graphics g){
+		super.paint(g);
+		Graphics2D obj = (Graphics2D) g;
+		//obj.setColor(Color.BLUE);
+		obj.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		obj.fillOval(x,y,30,30);
+	}
+	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Mini Game");
+		Game game = new Game();
+		frame.add(game);
 		frame.setSize(400, 400);
 		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		//so that the program exits/terminates on closing
+		
+		while(game.x < 400 && game.y < 400){
+			game.x++;
+			game.y++;
+			game.repaint();
+			try{
+				Thread.sleep(10);
+			}
+			catch(InterruptedException ie){
+				System.out.println(ie);
+			}
+		}
 	}
 }
